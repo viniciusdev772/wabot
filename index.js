efbf428adea8079eb5b78229bdff1c5e2cb18e5e
viniciusdev772 <viniciusdev37@gmail.com>
2024-02-18 21:35:37 -0300

    Atualizações de código

---
+++ index.js
@@ -0,0 +1,23 @@
+import venom from "venom-bot";
+
+venom
+  .create("minhaSessaoDeBot") // Nome da sessão
+  .then((client) => {
+    client.onMessage((message) => {
+      if (message.body === "conectar" && !message.isGroupMsg) {
+        client
+          .sendText(message.from, "Olá, tudo bem?")
+          .then(() =>
+            client.sendText(message.from, "Como posso ajudá-lo hoje?")
+          )
+          .then(() =>
+            client.sendText(
+              message.from,
+              "Fique à vontade para fazer perguntas."
+            )
+          )
+          .catch((error) => console.error("Erro ao enviar: ", error));
+      }
+    });
+  })
+  .catch((error) => console.log(error));
